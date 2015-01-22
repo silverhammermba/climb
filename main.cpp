9f8f12cd75fce657b2a84391e867de5d1b8a7052
Max <silverhammermba@gmail.com>
2015-01-22 17:13:45 -0500

    Add color changing stuff

--- main.cpp
+++ main.cpp
@@ -1,8 +1,16 @@
+#include <stdlib.h>
+#include <time.h>
+
 #include <SFML/Graphics.hpp>
 
 int main(int argc, char* argv[])
 {
 	sf::RenderWindow window {sf::VideoMode {800, 600}, "Test"};
+
+	srand(time(nullptr));
+
+	sf::Color background {0, 0, 0};
+	sf::Color new_color {0, 0, 0};
 
 	bool running = true;
 	while (running)
@@ -14,7 +22,29 @@ int main(int argc, char* argv[])
 				running = false;
 		}
 
-		window.clear();
+		// pick new background color if we're there
+		if (new_color == background)
+		{
+			new_color.r = rand() % 256;
+			new_color.g = rand() % 256;
+			new_color.b = rand() % 256;
+		}
+
+		// move towards new background color
+		if (new_color.r < background.r)
+			background.r -= rand() % 2;
+		else if (new_color.r > background.r)
+			background.r += rand() % 2;
+		else if (new_color.g < background.g)
+			background.g -= rand() % 2;
+		else if (new_color.g > background.g)
+			background.g += rand() % 2;
+		else if (new_color.b < background.b)
+			background.b -= rand() % 2;
+		else if (new_color.b > background.b)
+			background.b += rand() % 2;
+
+		window.clear(background);
 		window.display();
 	}
 
